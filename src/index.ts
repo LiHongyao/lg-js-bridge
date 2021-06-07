7abb9f8c88c8354ea9952cbf3ed52889febc96ed
lihongyao <lihy_online@163.com>
2021-06-07 23:09:59 +0800

    优化

--- src/index.ts
+++ src/index.ts
@@ -20,46 +20,49 @@ type PromptType = {
 };
 
 class JSBridge {
-  /**
-   * 判断iOS设备
-   */
-  private static isiOS() {
-    return /iPhone/i.test(window.navigator.userAgent);
-  }
-
-  /**
-   * 判断安卓设备
-   */
-  private static isAndroid() {
-    return /Linux|Android/i.test(window.navigator.userAgent);
+  private static getEnv() {
+    const _userAgent = window.navigator.userAgent;
+    if (/MicroMessenger/i.test(_userAgent)) {
+      return 'weixin';
+    } else if (/Linux|Android/i.test(_userAgent)) {
+      return 'android';
+    } else if (/iPhone/i.test(_userAgent)) {
+      return 'ios';
+    } else {
+      return 'unknown';
+    }
   }
 
   /**
    * 调用原生方法通信
    * @param options
    * options.fnName 原生方法名
-   * options.data 传递参数（对象类型）/如果没有，则传递null
+   * options.data H5传递给原生的参数/如果没有，则传递null
    * options.iOSPrompt 处理iOS同步返回
    */
   private static call(options: { fnName: string; data?: object; iOSPrompt?: PromptType }) {
-    if (JSBridge.isiOS()) {
-      try {
-        if (options.iOSPrompt) {
-          return prompt(JSON.stringify(options.iOSPrompt));
-        } else {
-          return window.webkit.messageHandlers[options.fnName].postMessage(
-            options.data ? JSON.stringify(options.data) : null,
-          );
+    switch (JSBridge.getEnv()) {
+      case 'ios':
+        try {
+          if (options.iOSPrompt) {
+            return prompt(JSON.stringify(options.iOSPrompt));
+          } else {
+            return window.webkit.messageHandlers[options.fnName].postMessage(
+              options.data ? JSON.stringify(options.data) : null,
+            );
+          }
+        } catch (err) {
+          console.log(err);
         }
-      } catch (err) {
-        console.log(err);
-      }
-    } else if (JSBridge.isAndroid()) {
-      try {
-        return window.js_android[options.fnName](options.data ? JSON.stringify(options.data) : null);
-      } catch (err) {
-        console.log(err);
-      }
+        break;
+      case 'android':
+        try {
+          return window.js_android[options.fnName](options.data ? JSON.stringify(options.data) : null);
+        } catch (err) {
+          console.log(err);
+        }
+        break;
+      default:
     }
   }
 
@@ -68,7 +71,7 @@ class JSBridge {
    */
 
   /**
-   * 跳转微信小程序
+   * 1. 跳转微信小程序
    * @param {*} options
    */
   public static launchMiniProgram(options: {
@@ -83,10 +86,10 @@ class JSBridge {
   }
 
   /**
-   * H5调用原生支付
+   * 2. H5调用原生支付
    */
   public static payment(params: {
-    callback: string /** 支付回调函数名 */;
+    callback: string /** 支付回调H5函数名 */;
     payType: string /** 支付类型，目前仅支持支付宝支付、微信支付、 熊猫支付 */;
     payStr: string /** 支付参数（这里需要和后台沟通，将原生拉起支付的参数以JSON字符串形式返回，到时直接传递给原生并由原生解析即可） */;
     orderNo?: string /** 订单号（有时原生调用支付回调函数之后，H5这边需要通过订单号查询支付状态，所以这里将订单号传给原生，原生在回调时作为参数回传给H5使用） */;
@@ -98,7 +101,7 @@ class JSBridge {
   }
 
   /**
-   * H5调用原生分享
+   * 3. H5调用原生分享
    */
   public static shareWith(options: {
     type: 0 | 1 | 2 | 3 | 4 /** 分享类型： 0 文字 / 1 图片 / 2 网页链接 / 3 视频连接 / 4 小程序 */;
@@ -116,7 +119,7 @@ class JSBridge {
   }
 
   /**
-   * 保存图片至手机相册
+   * 4. 保存图片至手机相册
    * @param images 图片集合/这里将图片的在线链接放入集合传递给原生进行保存
    */
   public static saveImages(images: string[]) {
@@ -126,7 +129,7 @@ class JSBridge {
     });
   }
   /**
-   * 保存视频至手机相册
+   * 5. 保存视频至手机相册
    * @param {string} videoUrls 视频地址集合/这里将视频的在线链接放入集合传递给原生进行保存
    */
   public static saveVideos(videoUrls: string[]) {
@@ -137,35 +140,34 @@ class JSBridge {
   }
 
   /**
-   * 通知原生返回上一页（原生pop控制器）
+   * 6. 通知原生返回上一页（原生pop控制器）
    */
   public static nativeBack() {
-    // JSBridge.call('nativeBack');
-    if (JSBridge.isiOS()) {
-      try {
-        window.webkit.messageHandlers.nativeBack.postMessage(null);
-      } catch (err) {
+    switch (JSBridge.getEnv()) {
+      case 'ios':
         try {
-          window.webkit.messageHandlers.gobackAPP.postMessage(null);
-        } catch (e) {
-          console.log(e);
+          window.webkit.messageHandlers.nativeBack.postMessage(null);
+        } catch (err) {
+          try {
+            window.webkit.messageHandlers.gobackAPP.postMessage(null);
+          } catch (e) {
+            console.log(e);
+          }
         }
-      }
-    } else if (JSBridge.isAndroid()) {
-      try {
-        window.js_android.nativeBack(null);
-      } catch (err) {
+        break;
+      case 'android':
         try {
           window.js_android.gobackAPP(null);
         } catch (e) {
           console.log(e);
         }
-      }
+        break;
+      default:
     }
   }
 
   /**
-   * 通知原生绑定微信
+   * 7. 通知原生绑定微信
    * @param callback 微信绑定之后的回调函数
    */
   public static bindWeChat(callback: string) {
@@ -175,7 +177,7 @@ class JSBridge {
     });
   }
   /**
-   * 通知原生打开微信
+   * 8. 通知原生打开微信
    */
   public static openWeChat() {
     JSBridge.call({
@@ -184,7 +186,7 @@ class JSBridge {
   }
 
   /**
-   * 通知原生定位
+   * 9. 通知原生定位
    * @param callback 原生定位成功以后调用H5回调函数，并将定位信息作为回调函数参数传递。
    */
   public static getLocation(callback: string) {
@@ -195,7 +197,7 @@ class JSBridge {
   }
 
   /**
-   * 从原生获取token。
+   * 10. 从原生获取token。
    *
    * iOS开发者注意：----- 此方法通过prompt触发，type类型为：GET_TOKEN
    */
